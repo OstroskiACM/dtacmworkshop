8ce7eea77fb71b240d577c9a508d20f362d248f1
Jason Ostroski <jason.ostroski@dynatrace.com>
2020-07-27 16:46:05 -0400

    Update deleteAll.sh

--- 5-Cleanup/deleteAll.sh
+++ 5-Cleanup/deleteAll.sh
@@ -2,5 +2,5 @@
 
 ../utils/deleteagConfiguration.sh
 
-gcloud container clusters delete acmworkshop --zone=us-central1-a -q
+gcloud container clusters delete ostroskiacmworkshop --zone=us-central1-a -q
 gcloud compute instances delete dtactivegate --zone=us-central1-a -q
